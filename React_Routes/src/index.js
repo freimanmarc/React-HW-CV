376582482ea22a9ae41b45814d8b27eb74ed3748
Marc Freiman <marcfb2@hotmail.com>
2018-07-27 09:18:44 -0400

    Upload homework

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
-
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
+import Nav from './components/Nav';
 
 // your code goes here
 class Home extends Component{
   render(){
     return(
-      // your code goes here
-    )
+      <BrowserRouter>
+<div>
+    <Route path = '/' component = {Nav} />
+    <Route path = '/Sleepy' component = {Sleepy} />
+    <Route path = '/Happy' component = {Happy} />
+    <Route path = '/Guilty' component = {Guilty} />
+</div>
+</BrowserRouter>)
   }
 }
 
